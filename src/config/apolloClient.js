92d64df2feb0f3df3a5a054a90ab4e490605ecc5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-10-30 17:11:58 +0200

    Update credentials from client

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: process.env.API_URI,
+  uri: process.env.REACT_APP_API_URI,
   cache: new InMemoryCache(),
   headers: {
-    'x-hasura-admin-secret': process.env.API_ADMIN_SECRET
+    'x-hasura-admin-secret': process.env.REACT_APP_API_ADMIN_SECRET
   }
 })
