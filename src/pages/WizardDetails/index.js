dc680876b75f8bacd4cd9ef556c47bd339f9a062
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-10-31 16:22:42 +0100

    Implement delete funcionally

--- src/pages/WizardDetails/index.js
+++ src/pages/WizardDetails/index.js
@@ -4,17 +4,27 @@ import { HStack, Stack, Flex, Text } from '@chakra-ui/layout'
 import { DeleteIcon, ArrowBackIcon } from '@chakra-ui/icons'
 import { Button } from '@chakra-ui/button'
 import { Helmet } from 'react-helmet'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useHistory, useParams } from 'react-router-dom'
 
 import { PageLayout } from '../../components/PageLayout'
 import { useGetWizardQuery } from '../../hooks/graphql/queries/useGetWizardQuery'
+import { useDeleteWizardMutation } from '../../hooks/graphql/mutations/useDeleteWizardMutation'
 import { WizardBadge } from '../../components/WizardBadge'
 import { useFetchPolicyStore } from '../../hooks/stores/useFetchPolicyStore'
 import { CommentsTable } from '../../components/CommentsTable'
+import { HOME_PAGE_PATH } from '../../constants/routesPaths'
 
 const textPlaceholderForSkeletons = 'Placeholder'
 
-function WizardDetailsHeader ({ isLoaded, houseName, wizardName }) {
+function WizardDetailsHeader ({ isLoaded, wizardId, houseName, wizardName }) {
+  const history = useHistory()
+
+  const [deleteWizard] = useDeleteWizardMutation({
+    onCompleted: () => history.push(HOME_PAGE_PATH)
+  })
+
+  const handleDeleteWizard = (id) => () => deleteWizard({ variables: { id } })
+
   return (
     <Flex width='100%' align='center'>
       <Stack
@@ -45,6 +55,7 @@ function WizardDetailsHeader ({ isLoaded, houseName, wizardName }) {
         <Button
           size='sm'
           variant='unstyled'
+          onClick={handleDeleteWizard(wizardId)}
         >
           <HStack
             color='gray.gradient2'
@@ -84,6 +95,7 @@ export function WizardDetails () {
   })
 
   const {
+    id: wizardId,
     name: wizardName,
     house: { name: houseName } = {},
     image_url: imageUrl,
@@ -100,6 +112,7 @@ export function WizardDetails () {
 
       <WizardDetailsHeader
         isLoaded={isLoaded}
+        wizardId={wizardId}
         houseName={houseName}
         wizardName={wizardName}
       />
