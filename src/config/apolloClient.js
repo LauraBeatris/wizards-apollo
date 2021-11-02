5f363d51d0df785b8b86a159677a2e9fe4d92329
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-02 08:15:27 +0100

    Customize key fields from wizard type

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,6 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-export const cache = new InMemoryCache()
+export const cache = new InMemoryCache({
+  typePolicies: {
+    wizard: {
+      keyFields: ['id', 'name', 'house', ['id', 'name']]
+    }
+  }
+})
 
 export const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URI,
