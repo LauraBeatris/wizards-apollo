ec75f6b61fd6e627423fa451138507646ac86a46
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-03 07:58:39 +0100

    Export cache to access methods

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+export const cache = new InMemoryCache()
+
 export const client = new ApolloClient({
   uri: process.env.REACT_APP_API_URI,
-  cache: new InMemoryCache(),
+  cache,
   headers: {
     'x-hasura-admin-secret': process.env.REACT_APP_API_ADMIN_SECRET
   }
