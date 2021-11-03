b49d2fe045818ce09fc550158d600c0b6f003907
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-03 09:03:00 +0100

    Update cache when creating wizard

--- src/hooks/graphql/mutations/useCreateWizardMutation/index.js
+++ src/hooks/graphql/mutations/useCreateWizardMutation/index.js
@@ -3,13 +3,38 @@ import gql from 'graphql-tag'
 
 const createWizardMutation = gql`
   mutation CreateWizard ($wizardData: wizard_insert_input!) {
-    insert_wizard_one(object: $wizardData) {
+    new_wizard: insert_wizard_one(object: $wizardData) {
       id
       name
     }
   }
 `
 
+const addWizardToCache = ({ cache, newWizard }) => {
+  cache.modify({
+    fields: {
+      wizard (existingWizards = []) {
+        const newWizardRef = cache.writeFragment({
+          data: newWizard,
+          fragment: gql`
+            fragment NewWizard on wizard {
+              id
+              name
+            }
+          `
+        })
+
+        return [...existingWizards, newWizardRef]
+      }
+    }
+  })
+}
+
 export function useCreateWizardMutation (mutationOptions) {
-  return useMutation(createWizardMutation, mutationOptions)
+  return useMutation(createWizardMutation, {
+    ...mutationOptions,
+    update (cache, { data: { new_wizard: newWizard } }) {
+      addWizardToCache({ cache, newWizard })
+    }
+  })
 }
