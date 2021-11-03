d46d4f47ee2e23cb6e65c9993b482c949696e6a4
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-03 08:45:14 +0100

    Update cache when deleting wizard

--- src/hooks/graphql/mutations/useDeleteWizardMutation/index.js
+++ src/hooks/graphql/mutations/useDeleteWizardMutation/index.js
@@ -3,13 +3,30 @@ import { useMutation } from '@apollo/client'
 
 const deleteWizardMutation = gql`
   mutation DeleteWizard ($id: Int!) {
-    delete_wizard_by_pk(id: $id) {
+    wizard: delete_wizard_by_pk(id: $id) {
       id
       name
     }
   }
 `
 
+const deleteWizardFromCache = ({ cache, wizard }) => {
+  cache.modify({
+    fields: {
+      wizard (cachedWizards, { readField }) {
+        return cachedWizards.filter(
+          commentRef => wizard.id !== readField('id', commentRef)
+        )
+      }
+    }
+  })
+}
+
 export function useDeleteWizardMutation (mutationOptions) {
-  return useMutation(deleteWizardMutation, mutationOptions)
+  return useMutation(deleteWizardMutation, {
+    ...mutationOptions,
+    update (cache, { data: { wizard } }) {
+      deleteWizardFromCache({ cache, wizard })
+    }
+  })
 }
