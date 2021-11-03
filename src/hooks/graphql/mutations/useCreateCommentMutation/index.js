7d5289288a480d019d54d9a107cebb2da61c4cdf
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-11-03 08:52:23 +0100

    Update cache when creating wizard comment

--- src/hooks/graphql/mutations/useCreateCommentMutation/index.js
+++ src/hooks/graphql/mutations/useCreateCommentMutation/index.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag'
 
 const createCommentMutation = gql`
   mutation CreateComment ($commentData: comments_insert_input!) {
-    insert_comments_one(object: $commentData) {
+    comment: insert_comments_one(object: $commentData) {
       id 
       text
       wizard_id
@@ -11,6 +11,33 @@ const createCommentMutation = gql`
   }
 `
 
+const addCommentToWizardOnCache = ({ cache, comment }) => {
+  cache.modify({
+    id: cache.identify({ __typename: 'wizard', id: comment.wizard_id }),
+    fields: {
+      comments (existingComments = []) {
+        const newCommentRef = cache.writeFragment({
+          data: comment,
+          fragment: gql`
+            fragment NewComment on comments {
+              id
+              text
+              wizard_id
+            }
+          `
+        })
+
+        return [...existingComments, newCommentRef]
+      }
+    }
+  })
+}
+
 export function useCreateCommentMutation (mutationOptions) {
-  return useMutation(createCommentMutation, mutationOptions)
+  return useMutation(createCommentMutation, {
+    ...mutationOptions,
+    update (cache, { data: { comment } }) {
+      addCommentToWizardOnCache({ cache, comment })
+    }
+  })
 }
